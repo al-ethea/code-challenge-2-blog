9d499847f4f82330912942fa9fe50c231b99dbfc
al-ethea <alethea.azka3@gmail.com>
2025-03-18 23:01:51 +0700

    update route.ts

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,7 +1,7 @@
 import Backendless from "@/lib/backendless";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const fetchBlog = await Backendless.Data.of("blogs").find();
     // console.log(fetchBlog);
